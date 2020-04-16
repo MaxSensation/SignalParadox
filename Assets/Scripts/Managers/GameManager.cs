f18959648bbf0386e056570c828bee9adb668e53
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-04-16 11:59:47 +0200

    Fixed PlayerPrefab

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,14 +8,12 @@ namespace Managers
     public class GameManager : MonoBehaviour
     {
         private GameObject _player;
-        private int totalCollectedMemos;
         private void Start()
         {
             DontDestroyOnLoad(this);
             SaveManager.Init();
             //Register Events
             EventHandler.RegisterListener<OnButtonStartEvent>(LoadLevel);
-            EventHandler.RegisterListener<OnTriggerMemoEvent>(MemoCollected);
         }
 
         private void LoadLevel(OnButtonStartEvent obj)
@@ -26,12 +24,7 @@ namespace Managers
         private void OnDestroy()
         {
             //Unregister Events
-            EventHandler.UnregisterListener<OnTriggerMemoEvent>(MemoCollected);
-        }
-
-        private void MemoCollected(OnTriggerMemoEvent obj)
-        {
-            totalCollectedMemos++;
+            EventHandler.UnregisterListener<OnButtonStartEvent>(LoadLevel);
         }
     }
 }
