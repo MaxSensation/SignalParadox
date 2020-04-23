0765bcce8e65ea0ac0288908aedefcf1926d6aac
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-04-23 21:39:33 +0200

    Added Animation Script and remade the player prefab

---
+++ Assets/Scripts/PlayerController/PlayerAnimatorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayerController
+{
+    public class PlayerAnimatorController : MonoBehaviour
+    {
+        private Animator _animator;
+        
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            _animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+            _animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+            if (Input.GetKeyDown(KeyCode.Space))
+                _animator.SetTrigger("jump");
+            
+        }
+    }
+}
