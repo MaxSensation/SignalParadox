1550e04c78cfda51bd2de3d25e3fae850d728fb3
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-04-28 21:32:01 +0200

    Moved a lot of textures and shit

--- Assets/Scripts/Triggers/TriggerLoadNextLevel.cs
+++ Assets/Scripts/Triggers/TriggerLoadNextLevel.cs
@@ -3,13 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class TriggerLoadNextLevel : MonoBehaviour
 {
-    public string levelToLoad;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(levelToLoad);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
