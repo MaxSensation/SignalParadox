e25b440978ba9cafc3902bf7b8897199c5942b59
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-04-30 21:01:54 +0200

    Added platform

---
+++ Assets/PlatformTrigger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlatformTrigger : MonoBehaviour
+{
+    private Animator _animator;
+    [SerializeField] private Material on;
+    [SerializeField] private Material off;
+    private MeshRenderer _meshRenderer;
+    private static readonly int IsPressed = Animator.StringToHash("IsPressed");
+    public UnityEvent isOn;
+    public UnityEvent isOff;
+
+    private void Awake()
+    {
+        var parent = transform.parent;
+        _animator = parent.GetComponent<Animator>();
+        _meshRenderer = parent.transform.Find("PlatformButton").GetComponent<MeshRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _animator.SetBool(IsPressed, true);
+            _meshRenderer.material = on;
+            isOn.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _animator.SetBool(IsPressed, false);
+            _meshRenderer.material = off;
+            isOff.Invoke();
+        }
+    }
+}
