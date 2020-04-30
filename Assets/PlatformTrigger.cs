0811513c1308ad34d003d247a23729ab371be594
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-04-30 22:11:23 +0200

    Fixed PlatformButton

--- Assets/PlatformTrigger.cs
+++ Assets/PlatformTrigger.cs
@@ -13,9 +13,11 @@ public class PlatformTrigger : MonoBehaviour
     public UnityEvent isOn;
     public UnityEvent isOff;
     public static Action<GameObject[]> onButtonPressed;
+    private int objectsOnButton;
 
     private void Awake()
     {
+        objectsOnButton = 0;
         var parent = transform.parent;
         _animator = parent.GetComponent<Animator>();
         _meshRenderer = parent.transform.Find("PlatformButton").GetComponent<MeshRenderer>();
@@ -23,23 +25,23 @@ public class PlatformTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
-        {
-            _animator.SetBool(IsPressed, true);
-            _meshRenderer.material = on;
-            isOn.Invoke();
-            onButtonPressed?.Invoke(interactables);
-        }
+        if (!other.CompareTag("Player") && !other.CompareTag("Enemy")) return;
+        objectsOnButton++;
+        if (objectsOnButton != 1) return;
+        _animator.SetBool(IsPressed, true);
+        _meshRenderer.material = @on;
+        isOn.Invoke();
+        onButtonPressed?.Invoke(interactables);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
-        {
-            _animator.SetBool(IsPressed, false);
-            _meshRenderer.material = off;
-            isOff.Invoke();
-            onButtonPressed?.Invoke(interactables);
-        }
+        if ((!other.CompareTag("Player") && !other.CompareTag("Enemy"))) return;
+        objectsOnButton--;
+        if (objectsOnButton != 0) return;
+        _animator.SetBool(IsPressed, false);
+        _meshRenderer.material = off;
+        isOff.Invoke();
+        onButtonPressed?.Invoke(interactables);
     }
 }
