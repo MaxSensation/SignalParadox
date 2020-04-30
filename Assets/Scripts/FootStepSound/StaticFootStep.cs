6755be949a2b9dd735208a0bf5df848a4748143f
Kezze16 <kez.fds@gmail.com>
2020-04-30 14:51:20 +0200

    Added 3 Scripts (FootStepScript)

---
+++ Assets/Scripts/FootStepSound/StaticFootStep.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticFootStep : MonoBehaviour
+{
+
+    private AudioSource source;
+    public AudioClip clip;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlaystaticFootstepSound()
+    {
+
+        source.PlayOneShot(clip);
+
+    }
+}
---
+++ Assets/Scripts/FootStepSound/SurfaceColliderType.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceColliderType : MonoBehaviour
+{
+
+    public enum Mode  {Default, Grass, Dirt}
+    public Mode terrainType;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public string GetTerrainType()
+    {
+        string typeString = "";
+
+        switch (terrainType) {
+
+            case Mode.Default:
+                typeString = "Default";
+                break;
+
+            case Mode.Grass:
+                typeString = "Grass";
+                break;
+
+            case Mode.Dirt:
+                typeString = "Dirt";
+                break;
+            default:
+                typeString = "";
+                break;
+                
+        }
+
+        return typeString;
+
+
+    }
+}
