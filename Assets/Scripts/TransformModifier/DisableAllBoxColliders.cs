fa699bf9882ba7d4ce6373037605d4a6ecd45fe4
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-05 22:17:22 +0200

    Fixed last puzzle

---
+++ Assets/Scripts/TransformModifier/DisableAllBoxColliders.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Interactables.Button;
+using UnityEngine;
+
+public class DisableAllBoxColliders : MonoBehaviour
+{
+    private void Awake()
+    {
+        ButtonController.onButtonPressed += Disable;
+    }
+
+    private void Disable(GameObject[] interactable)
+    {
+        if (interactable.Contains(gameObject))
+        {
+            foreach (var t in transform.GetComponentsInChildren<Transform>())
+            {
+                foreach (var b in t.GetComponentsInChildren<Transform>())
+                {
+                    if (b.name == "Trigger")
+                    {
+                        b.GetComponent<BoxCollider>().enabled = false;
+                    }
+                }
+            }
+        }
+    }
+}
