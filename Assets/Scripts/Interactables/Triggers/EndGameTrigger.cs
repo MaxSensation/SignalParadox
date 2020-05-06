78912594addfb0d907c8f1d0e2c03de771600850
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-06 18:30:16 +0200

    Added EndGameTriggerScripts

---
+++ Assets/Scripts/Interactables/Triggers/EndGameTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EndGameTrigger : MonoBehaviour
+{
+
+    [SerializeField] private Animator textAnimation;
+    [SerializeField] private Animator imageAnimation;
+    [SerializeField] private float endGameDelay;
+    private WaitForSeconds _endGameDelay;
+
+    private void Awake()
+    {
+        _endGameDelay = new WaitForSeconds(endGameDelay);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            EndGame();
+    }
+
+    private IEnumerator EndGameTimer()
+    {
+        yield return _endGameDelay;
+    }
+
+    private void EndGame()
+    {
+        StartCoroutine("EndGameTimer");
+        textAnimation.SetBool("ReachedEnd", true);
+        imageAnimation.SetBool("ReachedEnd", true);
+    }
+}
