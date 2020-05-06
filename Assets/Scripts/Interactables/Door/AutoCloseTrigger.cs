a2e18eb9af2ce5b5c922cbc9b5e6037d33269941
Berzan3 <63124461+Berzan3@users.noreply.github.com>
2020-05-06 21:42:12 +0200

    Gjorde AutoClosing door

---
+++ Assets/Scripts/Interactables/Door/AutoCloseTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCloseTrigger : MonoBehaviour
+{
+    public Action OnAutoClose;
+    private bool _hasClosed;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && !_hasClosed)
+        {
+            OnAutoClose?.Invoke();
+            _hasClosed = true;
+        }
+    }
+}
