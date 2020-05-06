bca7d8a52641b9f1e1841ce33b7fe68de0205fb9
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-06 15:30:10 +0200

    Added Exit Button

---
+++ Assets/Scripts/Menu/ExitGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour
+{
+
+    private Button _button;
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(EndGame);
+    }
+
+    private void EndGame()
+    {
+        print("Exited Game");
+        Application.Quit();
+    }
+}
