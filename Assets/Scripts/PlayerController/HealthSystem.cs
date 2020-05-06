19ca55cc6e4f51d75f4806fc5bef348267996cea
Berzan3 <63124461+Berzan3@users.noreply.github.com>
2020-05-06 17:55:19 +0200

    uppdaterade healthsystem med enums som berättar vilken damagetype som togs emot och playeranimator controller ansvarar för vilken som ska spelas

--- Assets/Scripts/PlayerController/HealthSystem.cs
+++ Assets/Scripts/PlayerController/HealthSystem.cs
@@ -1,5 +1,6 @@
 ﻿//Main author: Maximiliam Rosén
 
+using Interactables.Traps;
 using System;
 using UnityEngine;
 
@@ -9,23 +10,58 @@ namespace PlayerController
     {
         [SerializeField] private int maxHealth;
         public static Action<int> onPlayerTakeDamageEvent;
+        public static Action<DamageType> OnPlayerDeath;
         public int CurrentHealth { get; private set; }
+        public enum DamageType { Laser, Steam, Bodytrapper, Charger }
+
+        private void Awake()
+        {
+            SteamController.onSteamDamage += SteamDamage;
+            LaserController.onLaserDeath += LaserDamage;
+        }
 
         private void Start()
         {
             CurrentHealth = maxHealth;
         }
+
+        public void SteamDamage(GameObject o)
+        {
+            if (o != gameObject) return;
+            TakeDamage(DamageType.Steam);
+        }
+
+        public void BodyTrapperDamage(GameObject o)
+        {
+            if (o != gameObject) return;
+            TakeDamage(DamageType.Bodytrapper);
+        }
+
+        public void LaserDamage(GameObject o)
+        {
+            if (o != gameObject) return;
+            CurrentHealth = 1;
+            TakeDamage(DamageType.Laser);
+        }
         
-        public void TakeDamage()
+        public void TakeDamage(DamageType dT)
         {
             CurrentHealth--;
             onPlayerTakeDamageEvent?.Invoke(CurrentHealth);
+            if (CurrentHealth <= 0)
+                OnPlayerDeath?.Invoke(dT);
             Debug.Log("Damaged Player");
         }
 
         public void SetHealth(int health)
         {
             CurrentHealth = health;
+        }
+
+        private void OnDestroy()
+        {
+            SteamController.onSteamDamage -= SteamDamage;
+            LaserController.onLaserDeath -= LaserDamage;
         }
     }
 }
