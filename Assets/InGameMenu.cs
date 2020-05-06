75640efc6f2d3f1a722107d19eb457cb3ae835a1
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-06 15:26:03 +0200

    Fixed Ingame Menu Logic

---
+++ Assets/InGameMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class InGameMenu : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    
+    private void OnDisable()
+    {
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnDestroy()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
