34f1b7d039c5e88fd96387a694805ac4e14f9156
Berzan3 <63124461+Berzan3@users.noreply.github.com>
2020-05-12 16:08:04 +0200

    Fixade så man ser granaten i handen när man har en

---
+++ Assets/Scripts/DecoyGrenade/PropDecoyGrenade.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PropDecoyGrenade : MonoBehaviour
+{
+    private MeshRenderer propGrenade; 
+
+    private void Awake()
+    {
+        propGrenade = GetComponent<MeshRenderer>();
+        if (propGrenade.enabled)
+            propGrenade.enabled = false;
+        PickupDecoyGrenade.onGrenadePickup += Activate;
+        ThrowDecoyGrenade.OnThrowEvent += Deactivate;
+    }
+
+    private void Activate(int obj)
+    {
+        propGrenade.enabled = true;
+    }
+
+    private void Deactivate()
+    {
+        propGrenade.enabled = false;
+    }
+
+    private void OnDestroy()
+    {
+        PickupDecoyGrenade.onGrenadePickup -= Activate;
+        ThrowDecoyGrenade.OnThrowEvent -= Deactivate;
+    }
+}
