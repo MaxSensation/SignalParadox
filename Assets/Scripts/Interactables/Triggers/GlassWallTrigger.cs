7e34a2934a3f6871c65fd8dbf5ca48ef9c3cf53e
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-12 15:50:03 +0200

    Added GlasWallTrigger

---
+++ Assets/Scripts/Interactables/Triggers/GlassWallTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class GlassWallTrigger : MonoBehaviour
+{
+    private BoxCollider[] _colliders;
+    private Animator _animator;
+    private AudioSource _audioSource;
+    private void Start()
+    {
+        _colliders = GetComponents<BoxCollider>();
+        _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Enemy")) return;
+        _colliders.ToList().ForEach(i => i.enabled = false);
+        _animator.SetTrigger("GlassBreak");
+        _audioSource.Play();
+    }
+}
