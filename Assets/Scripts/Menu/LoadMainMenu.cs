414992de7e0a85157bf6badf56461d713950c7c2
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-17 19:20:25 +0200

    Added Intro!

---
+++ Assets/Scripts/Menu/LoadMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class LoadMainMenu : MonoBehaviour
+{
+    private VideoPlayer _videoPlayer;
+
+    private void Awake()
+    {
+        _videoPlayer = GetComponent<VideoPlayer>();
+        _videoPlayer.loopPointReached += LoadMainMenuScene;
+    }
+
+    private void LoadMainMenuScene(VideoPlayer source)
+    {
+        SceneManager.LoadScene("GameMenu");
+    }
+}
