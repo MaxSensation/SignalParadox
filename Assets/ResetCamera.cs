5fb9285cf3240a845f3ad27dbfc244f371af93fc
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-18 19:34:12 +0200

    Added Intro and Cinemachine

---
+++ Assets/ResetCamera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class ResetCamera : MonoBehaviour
+{
+    [SerializeField] private Vector3 _cameraValues;
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    public void Reset()
+    {
+        var cameraTransform = _camera.transform;
+        cameraTransform.localPosition = _cameraValues;
+        cameraTransform.rotation = new Quaternion();
+    }
+}
