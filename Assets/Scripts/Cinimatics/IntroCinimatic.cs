205656dbe55bbcf8591b4168b571bbb288b3c231
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-19 22:05:32 +0200

    Added VisualEffects

--- Assets/Scripts/Cinimatics/IntroCinimatic.cs
+++ Assets/Scripts/Cinimatics/IntroCinimatic.cs
@@ -1,26 +1,16 @@
-﻿using Cinemachine;
-using SaveSystem;
+﻿using SaveSystem;
 using UnityEngine;
 using UnityEngine.Playables;
 
 public class IntroCinimatic : MonoBehaviour
 {
-    [SerializeField] private GameObject playerMesh;
-    [SerializeField] private GameObject cinimaticPlayer;
-    [SerializeField] private GameObject camera;
     private PlayableDirector _playableDirector;
     private void Start()
     {
         _playableDirector = GetComponent<PlayableDirector>();
+        if (SaveManager.WorldEventsData == null) return;
         if (!SaveManager.WorldEventsData.CinematicHasPlayed)
             _playableDirector.Play();
-        else
-        {
-            playerMesh.SetActive(true);
-            playerMesh.GetComponent<TurnWithCamera>().enabled = true;
-            camera.GetComponent<CinemachineBrain>().enabled = false;
-            cinimaticPlayer.SetActive(false);
-        }
         SaveManager.WorldEventsData.CinematicHasPlayed = true;
     }
 }
