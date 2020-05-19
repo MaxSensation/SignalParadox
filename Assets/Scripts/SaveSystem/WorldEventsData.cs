b914c3f39138dae42e3f6daf7c96db57d6a3af39
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-19 14:42:05 +0200

    Fixed some Cinimatic Scripts

--- Assets/Scripts/SaveSystem/WorldEventsData.cs
+++ Assets/Scripts/SaveSystem/WorldEventsData.cs
@@ -7,6 +7,9 @@ namespace SaveSystem
     [Serializable]
     public class WorldEventsData
     {
+        //Level 1 Cinematic has played
+        public bool CinematicHasPlayed { get; set; }
+        
         //Level 3 Special Events
         public bool PuzzleLabyrinthCompleted { get; set; }
         public bool PuzzleGlassRoomCompleted { get; set; }
