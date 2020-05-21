d39f772e404093a47d59e3124dc36a5a8d69bf29
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-21 23:03:24 +0200

    Fixed materials

--- Assets/DefualtMaterialChanger.cs
+++ Assets/DefualtMaterialChanger.cs
@@ -1,23 +1,45 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
-[ExecuteInEditMode]
-public class DefualtMaterialChanger : MonoBehaviour
+public class DefaultMaterialChanger : EditorWindow
 {
-    [SerializeField] private Material materialToChangeTo;
+    private Material newMaterial;
+    private string nameStartWith;
+    
+    [MenuItem("Window/MaterialChanger")]
+    public static void ShowWindow() => GetWindow<DefaultMaterialChanger>("MaterialChanger");
 
-    [ContextMenu("Change")]
-    private void Change()
+    private void OnGUI()
+    {
+        newMaterial = (Material)EditorGUILayout.ObjectField("New Material", newMaterial, typeof(Material));
+        GUILayout.Label("Change all Default-Material to new Material");
+        if (GUILayout.Button("Change"))
+            ChangeMaterialsStartingWith("Default-Material");
+        GUILayout.Label("Change all materials starting with string");
+        nameStartWith = EditorGUILayout.TextField("Name starting with:", nameStartWith);
+        if (GUILayout.Button("Change"))
+            ChangeMaterialsStartingWith(nameStartWith);
+    }
+
+    private void ChangeMaterialsStartingWith(string materialName)
     {
         object[] obj = FindSceneObjectsOfType(typeof (GameObject));
         var counter = 0;
         foreach (var o in obj)
         {
             var g = (GameObject) o;
-            var m = g.GetComponent<MeshRenderer>();
-            if (m == null || m.sharedMaterial == null || m.sharedMaterial.name != "Default-Material") continue;
-            m.material = materialToChangeTo;
-            counter++;
+            var r = g.GetComponent<Renderer>();
+            if (r == null || r.sharedMaterials.Length <= 0) continue;
+            var materials = r.sharedMaterials;
+            for (var i = 0; i < r.materials.Length; i++)
+            {
+                if (!r.materials[i].name.StartsWith(materialName)) continue;
+                Debug.Log("Changed material: " + r.materials[i].name + " to material: " + newMaterial);
+                materials[i] = newMaterial;
+                counter++;
+            }
+            r.sharedMaterials = materials;
         }
-        Debug.Log(counter);
+        Debug.Log("Total of: " + counter + " has been changed to the new material: " + newMaterial.name + " from all materials in the scene starting with: " + materialName + ".");
     }
 }
