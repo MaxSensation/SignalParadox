2f6b29ff618c23d9ddf37912f0023bb40bac7d41
MaximiliamRosén <karlsson.maximiliam@gmail.com>
2020-05-21 15:51:43 +0200

    Fixed URP

---
+++ Assets/DefualtMaterialChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DefualtMaterialChanger : MonoBehaviour
+{
+    [SerializeField] private Material materialToChangeTo;
+
+    [ContextMenu("Change")]
+    private void Change()
+    {
+        object[] obj = FindSceneObjectsOfType(typeof (GameObject));
+        var counter = 0;
+        foreach (var o in obj)
+        {
+            var g = (GameObject) o;
+            var m = g.GetComponent<MeshRenderer>();
+            if (m == null || m.sharedMaterial == null || m.sharedMaterial.name != "Default-Material") continue;
+            m.material = materialToChangeTo;
+            counter++;
+        }
+        Debug.Log(counter);
+    }
+}
