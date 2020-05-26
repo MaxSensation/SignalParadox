58457324e66df2233617d140f17a998819bf88ad
MaxSensation <karlsson.maximiliam@gmail.com>
2020-05-26 22:53:38 +0200

    Refactor TriggerMemo #21

--- Assets/Scripts/Interactables/Triggers/TriggerMemo.cs
+++ Assets/Scripts/Interactables/Triggers/TriggerMemo.cs
@@ -9,23 +9,17 @@ namespace Interactables.Triggers
     {
         [SerializeField] private AudioClip memo;
         private bool hasPlayed;
-        private AudioSource _audioSource;
-        public static Action<AudioClip> onMemoPickup;
+        private AudioSource ringingSound;
+        public static Action<AudioClip> onMemoPickupEvent;
 
-        private void Start()
-        {
-            _audioSource = GetComponent<AudioSource>();
-        }
+        private void Start() => ringingSound = GetComponent<AudioSource>();
 
         private void OnTriggerEnter(Collider other)
         {
-            if (!hasPlayed && other.CompareTag("Player"))
-            {
-                Debug.Log("Memo Found");
-                hasPlayed = true;
-                onMemoPickup?.Invoke(memo);
-                _audioSource.Stop();
-            }
+            if (hasPlayed || !other.CompareTag("Player")) return;
+            hasPlayed = true;
+            onMemoPickupEvent?.Invoke(memo);
+            ringingSound.Stop();
         }
     }
 }
