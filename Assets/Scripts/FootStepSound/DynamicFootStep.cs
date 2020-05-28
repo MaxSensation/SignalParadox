a614e2cf7561901332d70135be4e575838c2d5d9
Berzan3 <63124461+Berzan3@users.noreply.github.com>
2020-05-28 22:40:02 +0200

    close #26  dynamicFootStep refactor

--- Assets/Scripts/FootStepSound/DynamicFootStep.cs
+++ Assets/Scripts/FootStepSound/DynamicFootStep.cs
@@ -1,116 +1,56 @@
 ﻿//Main author: Ferreira Dos Santos Keziah
+//Secondary author: Andreas Berzelius
 
 using UnityEngine;
 using Player.PlayerStateMachine;
+using System.Collections.Generic;
+
 namespace FootStepSound
 {
     public class DynamicFootStep : MonoBehaviour
     {
-
-        private AudioSource _source;
-        [SerializeField] private AudioClip Footsteps;
-        [SerializeField] private AudioClip Glass;
-        [SerializeField] private AudioClip Metal;
-
+        [SerializeField] private AudioClip[] surfaceSounds;
+        [SerializeField] private float defaultVolume = 0.2f;
+        [SerializeField] private float crouchVolume = 0.1f;
+        private Dictionary<SurfaceColliderType.SurfaceTypes, AudioClip> soundDictionary;
+        private AudioSource audiosource;
         private double time;
         private float filterTime;
-        private bool hasEnterd;
-
-
-        private string colliderType;
-
-
-
-        private void Start()
-        {
-            _source = GetComponent<AudioSource>();
-            _source.volume = 0.2f;
-            time = AudioSettings.dspTime;
-            filterTime = 0.2f;
-
-
-        }
+        private SurfaceColliderType.SurfaceTypes currentSurfaceType;
 
         private void Awake()
         {
-
-            CrouchState.onEnteredCrouchEvent += EnteredCrouch;
-            CrouchState.onExitCrouchEvent += ExitedCrouch;
-
-
+            soundDictionary = new Dictionary<SurfaceColliderType.SurfaceTypes, AudioClip>();
+            soundDictionary.Add(SurfaceColliderType.SurfaceTypes.Metal, surfaceSounds[0]);
+            soundDictionary.Add(SurfaceColliderType.SurfaceTypes.Stairs, surfaceSounds[1]);
+            soundDictionary.Add(SurfaceColliderType.SurfaceTypes.Glass, surfaceSounds[2]);
+            currentSurfaceType = SurfaceColliderType.SurfaceTypes.Metal;
+            audiosource = GetComponent<AudioSource>();
+            audiosource.volume = 0.2f;
+            time = AudioSettings.dspTime;
+            filterTime = 0.2f;
+            CrouchState.onEnteredCrouchEvent += OnEnteredCrouch;
+            CrouchState.onExitCrouchEvent += OnExitedCrouch;
+            SurfaceColliderType.onEnteredSurfaceZoneEvent += surfaceType => currentSurfaceType = surfaceType;
         }
 
         private void OnDestroy()
         {
-            CrouchState.onEnteredCrouchEvent -= EnteredCrouch;
-            CrouchState.onExitCrouchEvent -= ExitedCrouch;
+            CrouchState.onEnteredCrouchEvent -= OnEnteredCrouch;
+            CrouchState.onExitCrouchEvent -= OnExitedCrouch;
+            SurfaceColliderType.onEnteredSurfaceZoneEvent -= surfaceType => currentSurfaceType = surfaceType;
         }
 
-        private void EnteredCrouch()
+        private void OnEnteredCrouch() => audiosource.volume = crouchVolume;
+
+        private void OnExitedCrouch() => audiosource.volume = defaultVolume;
+
+
+        private void PlayFootstepSound()
         {
-            _source.volume = 0.01f;
+            if (AudioSettings.dspTime < time + filterTime) return;
+            time = AudioSettings.dspTime;
+            audiosource.PlayOneShot(soundDictionary[currentSurfaceType]);
         }
-
-        private void ExitedCrouch()
-        {
-            _source.volume = 0.2f;
-        }
-
-        private void OnTriggerEnter(Collider col)
-        {
-
-            if (col.CompareTag("FootSounds"))
-            {
-                hasEnterd = true;
-                var act = col.gameObject.GetComponent<Collider>().gameObject.GetComponent<SurfaceColliderType>();
-                if (act)
-                    colliderType = act.GetTerrainType();
-            }
-
-
-
-        }
-
-        private void OnTriggerExit(Collider col)
-        {
-            if (col.CompareTag("FootSounds"))
-            {
-
-                hasEnterd = false;
-
-            }
-
-
-        }
-
-
-        public void PlayFootstepSound()
-        {
-
-            if (hasEnterd)
-            {
-                if (AudioSettings.dspTime < time + filterTime)
-
-                    return;
-                time = AudioSettings.dspTime;
-                switch (colliderType)
-                {
-                    case "Footsteps":
-                        _source.PlayOneShot(Footsteps);
-                        break;
-                    case "Grass":
-                        _source.PlayOneShot(Glass);
-                        break;
-                    case "Dirt":
-                        _source.PlayOneShot(Metal);
-                        break;
-                }
-
-            }
-        }
-
-
-
-
     }
 }
