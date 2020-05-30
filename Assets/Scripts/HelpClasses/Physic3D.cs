45cdec19bf02068476d51c8fa25f0919889aa5a5
Maximiliam Rosén <Karlsson.maximiliam@gmail.com>
2020-05-30 15:42:27 +0200

    Refactord HelpClasses

--- Assets/Scripts/HelpClasses/Physic3D.cs
+++ Assets/Scripts/HelpClasses/Physic3D.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public static class Physic3D
 {
-    private static float _gravityCoefficient;
-    private static float _airResistant;
+    private static float gravityCoefficient;
+    private static float airResistant;
 
     public static Vector3 GetNormalForce(Vector3 velocity, Vector3 normal)
     {
@@ -23,7 +23,7 @@ public static class Physic3D
 
     public static float GetAirResistant()
     {
-        return Mathf.Pow(1 - _airResistant, Time.deltaTime);
+        return Mathf.Pow(1 - airResistant, Time.deltaTime);
     }
 
     public static Vector3 GetAcceleration(Vector3 velocity, float accelerationSpeedCoefficient)
@@ -47,12 +47,12 @@ public static class Physic3D
 
     public static Vector3 GetGravity()
     {
-        return Vector3.down * (_gravityCoefficient * Time.deltaTime);
+        return Vector3.down * (gravityCoefficient * Time.deltaTime);
     }
 
     public static void LoadWorldParameters(PhysicsWorld physicsWorld)
     {
-        _airResistant = physicsWorld.airResistant;
-        _gravityCoefficient = physicsWorld.gravityCoefficient;
+        airResistant = physicsWorld.airResistant;
+        gravityCoefficient = physicsWorld.gravityCoefficient;
     }
 }
--- Assets/Scripts/HelpClasses/StateMachine.cs
+++ Assets/Scripts/HelpClasses/StateMachine.cs
@@ -5,10 +5,10 @@ using System;
 
 public class StateMachine
 {
-    private State _currentState;
-    private State _queuedState;
-    private Stack<State> _automaton = new Stack<State>();
-    private readonly Dictionary<Type, State> _stateDictionary = new Dictionary<Type, State>();
+    private State currentState;
+    private State queuedState;
+    private Stack<State> automaton = new Stack<State>();
+    private readonly Dictionary<Type, State> stateDictionary = new Dictionary<Type, State>();
 
     public StateMachine(object controller, State[] states)
     {
@@ -17,49 +17,42 @@ public class StateMachine
             var instance = UnityEngine.Object.Instantiate(state);
             instance.owner = controller;
             instance.stateMachine = this;
-            _stateDictionary.Add(instance.GetType(), instance);
+            stateDictionary.Add(instance.GetType(), instance);
             
-            if (_currentState == null)
-                _currentState = instance;
+            if (currentState == null)
+                currentState = instance;
         }
-        if (_currentState != null)
-            _currentState.Enter();
+        if (currentState != null)
+            currentState.Enter();
     }
 
     public void TransitionTo<T>() where T : State
     {
-        _queuedState = _stateDictionary[typeof(T)];
+        queuedState = stateDictionary[typeof(T)];
     }
 
     public void StackState<T>() where T : State
     {
-        _automaton.Push(_stateDictionary[typeof(T)]);
+        automaton.Push(stateDictionary[typeof(T)]);
     }
     
     public void UnStackState()
     {
-        _queuedState = _automaton.Pop();
+        queuedState = automaton.Pop();
     }
     
     public void Run()
     {
         UpdateState();
-        _currentState.Run();
-    }
-
-    public State GetCurrentState()
-    {
-        return _currentState;
+        currentState.Run();
     }
 
     private void UpdateState()
     {
-        if (_queuedState != null && _queuedState != _currentState)
-        {
-            if (_currentState != null)
-                _currentState.Exit();
-            _currentState = _queuedState;
-            _currentState.Enter();
-        }
+        if (queuedState == null || queuedState == currentState) return;
+        if (currentState != null)
+            currentState.Exit();
+        currentState = queuedState;
+        currentState.Enter();
     }
 }
