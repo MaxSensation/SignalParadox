3bebe271da856d7be46e2accb261fd529d114292
Maximiliam Rosén <Karlsson.maximiliam@gmail.com>
2020-06-02 20:56:18 +0200

    Fixed bug with triggerloadnextlevel

--- Assets/Scripts/Interactables/Triggers/Events/TriggerLoadNextLevel.cs
+++ Assets/Scripts/Interactables/Triggers/Events/TriggerLoadNextLevel.cs
@@ -1,51 +1,52 @@
 ﻿//Main author: Maximiliam Rosén
+//Secondary author: Andreas Berzelius
 
 using System;
 using Player;
 using SaveSystem;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using UnityEngine.Audio;
 
 namespace Interactables.Triggers.Events
 {
     public class TriggerLoadNextLevel : MonoBehaviour
     {
-        [SerializeField] [Tooltip("the black screen fading in between levels")] private GameObject loadingScreenObject;
+        [SerializeField] [Tooltip("the black screen fading in between levels")] private Animator loadingScreenAnimator;
         [SerializeField] private AudioMixer audioMixer;
         private AudioSource audioSource;
         private PlayerController player;
-        private Animator loadingScreenAnimator;
-        private Image loadingScreenImage;
         public static Action<PlayerData> onTriggerdNextLevelEvent;
+        private bool isActivated;
 
         private void Awake()
         {
             player = FindObjectOfType<PlayerController>();
-            loadingScreenAnimator = loadingScreenObject.GetComponent<Animator>();
-            loadingScreenImage = loadingScreenObject.GetComponent<Image>();
             audioSource = GetComponent<AudioSource>();
+        }
+
+
+        private void Update()
+        {
+            if (isActivated && !audioSource.isPlaying)
+                LoadNextLevel();
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("Player"))
-            {
-                var playerData = new PlayerData(other.gameObject);
-                onTriggerdNextLevelEvent?.Invoke(playerData);
-                Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Locked;
-                player.Velocity = Vector3.zero;
-                player.InCinematic = true;
-                loadingScreenAnimator.SetBool("ReachedEnd", true);
-                audioMixer.FindSnapshot("LevelTransition").TransitionTo(0.1f);
-                audioSource.Play();
-                if (loadingScreenImage.color.a == 1)
-                    LoadNextLevel();
-            }
+            if (!other.CompareTag("Player")) return;
+            var playerData = new PlayerData(other.gameObject);
+            onTriggerdNextLevelEvent?.Invoke(playerData);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            player.Velocity = Vector3.zero;
+            player.InCinematic = true;
+            loadingScreenAnimator.SetBool("ReachedEnd", true);
+            audioMixer.FindSnapshot("LevelTransition").TransitionTo(0.1f);
+            audioSource.Play();
+            isActivated = true;
         }
 
-        public void LoadNextLevel() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        private static void LoadNextLevel() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
